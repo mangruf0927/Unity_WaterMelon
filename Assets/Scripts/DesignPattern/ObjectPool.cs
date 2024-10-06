fd7a6ca9ca7add8a3b8bea2ce7e574e816435597
mangruf0927 <pinky0927@naver.com>
2024-10-06 21:48:24 +0900

    Update : ObjectPool 여러 종류의 오브젝트를 풀링할 수 있게 수정

--- Assets/Scripts/DesignPattern/ObjectPool.cs
+++ Assets/Scripts/DesignPattern/ObjectPool.cs
@@ -2,57 +2,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : Singleton<ObjectPool>
-{    
-    [Header("초기 풀 사이즈")]
-    public int poolSize = 20; 
-    
-    [SerializeField]
-    private GameObject objectPrefab;
+{
+    private List<Queue<GameObject>> poolList = new List<Queue<GameObject>>();
 
-    private Queue<GameObject> poolQueue = new Queue<GameObject>(); // 리스트 보다 Queue가 더 효율적
 
-    private void Awake() 
-    { 
-        if(objectPrefab == null) 
-        {
-            Debug.LogError("ObjectPrefab이 할당되지 않았습니다");
-        }
-
-        for(int i = 0; i < poolSize; i++)
-        {
-            GameObject obj = CreatePool();
-            poolQueue.Enqueue(obj);
-        }   
-    }
-
-    public GameObject GetFromPool()
+    public GameObject GetFromPool(GameObject gameObject, PoolTypeEnums poolType)
     {
-        if(poolQueue.Count > 0)
+        if (poolList[(int)poolType].Count > 0)
         {
-            GameObject obj = poolQueue.Dequeue();
+            GameObject obj = poolList[(int)poolType].Dequeue();
             obj.gameObject.SetActive(true);
-            // obj.transform.SetParent(null); (필요 시) 부모 해제
             return obj;
         }
         else
         {
-            // 필요시 오브젝트 생성
-            GameObject obj = CreatePool();
+            GameObject obj = CreatePool(gameObject);
             obj.SetActive(true);
             return obj;
         }
     }
 
-    public GameObject CreatePool()
+    public GameObject CreatePool(GameObject gameObject)
     {
-        GameObject obj = Instantiate(objectPrefab, this.transform);
+        GameObject obj = Instantiate(gameObject, this.transform);
         obj.SetActive(false);
         return obj;
     }
 
-    public void ReturnToPool(GameObject obj)
+    public void ReturnToPool(GameObject obj, PoolTypeEnums poolType)
     {
         obj.SetActive(false);
-        poolQueue.Enqueue(obj);
+        poolList[(int)poolType].Enqueue(obj);
     }
 }
---
+++ Assets/Scripts/Enums/PoolTypeEnums.cs
@@ -0,0 +1,5 @@
+public enum PoolTypeEnums
+{
+    DONGLE,
+    PARTICLE
+}
