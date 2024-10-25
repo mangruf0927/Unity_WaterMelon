d6e7ae72dfe242e686c77ee06995b88500f17eeb
mangruf0927 <pinky0927@naver.com>
2024-10-26 02:15:19 +0900

    Update : 상수 값 수정 및 동글이 X축 경계 로직 개선

--- Assets/Scripts/Dongle/DongleController.cs
+++ Assets/Scripts/Dongle/DongleController.cs
@@ -6,6 +6,7 @@ public class DongleController : MonoBehaviour, ISubject
     [SerializeField]    public Rigidbody2D rigid;
     [SerializeField]    private CircleCollider2D dongleCollider;
     [SerializeField]    private Animator animator;
+    [SerializeField]    private SpriteRenderer sprite;
     
     public int dongleLevel;
     public int dongleMaxLevel;
@@ -33,7 +34,7 @@ public class DongleController : MonoBehaviour, ISubject
     {
         if(isTouch)
         {
-            donglePosition.x = Mathf.Clamp(pos.x, GameConstants.LEFT_BOUNDARY + transform.localScale.x / 2f, GameConstants.RIGHT_BOUNDARY - transform.localScale.x / 2f);
+            donglePosition.x = Mathf.Clamp(pos.x, GameConstants.LEFT_BOUNDARY + sprite.bounds.size.x / 2f, GameConstants.RIGHT_BOUNDARY - sprite.bounds.size.x / 2f);
             donglePosition.y = GameConstants.DONGLE_HEIGHT;
 
             transform.position = Vector2.Lerp(transform.position, donglePosition, 0.2f);
--- Assets/Scripts/Utility/CameraScaler.cs
+++ Assets/Scripts/Utility/CameraScaler.cs
@@ -6,7 +6,7 @@ public class CameraScaler : MonoBehaviour
 
     private float referenceWidth = 1080f; // 기준 가로 해상도
     private float referenceHeight = 1920f; // 기준 세로 해상도
-    private float referenceOrthographicSize = 8f; // 기준 카메라 사이즈
+    private float referenceOrthographicSize = 6f; // 기준 카메라 사이즈
 
     private void Start()
     {
