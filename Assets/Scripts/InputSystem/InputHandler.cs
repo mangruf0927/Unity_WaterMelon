d1649b9b9244933bbe01ef4682cce444f49db06a
mangruf0927 <pinky0927@naver.com>
2024-11-04 18:10:43 +0900

    Update

--- Assets/Scripts/InputSystem/InputHandler.cs
+++ Assets/Scripts/InputSystem/InputHandler.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
@@ -17,44 +18,44 @@ public class InputHandler : MonoBehaviour
             Touch touch = Input.GetTouch(0);  // 첫 번째 터치 가져오기
 
             // 터치 시작 상태일 때
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == UnityEngine.TouchPhase.Began)
             {
                 OnTouchStart?.Invoke(true);  
             }
             // 터치가 유지되거나 움직였을 때
-            if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            if (touch.phase == UnityEngine.TouchPhase.Moved || touch.phase == UnityEngine.TouchPhase.Stationary)
             {
                 Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
                 OnTouchStay?.Invoke(touchPosition);
             }
             // 터치가 끝났을 때
-            if (touch.phase == TouchPhase.Ended)
+            if (touch.phase == UnityEngine.TouchPhase.Ended)
             {
                 OnTouchEnd?.Invoke(false);  
             }
         }
         
-        #if UNITY_EDITOR       
-        if(!IsClickUI())
-        { 
-            // 마우스 클릭 시작 시
-            if (Input.GetMouseButtonDown(0))
-            {
-                OnTouchStart?.Invoke(true);  // 마우스 클릭 시작 시 isTouch = true 전달
-            }
-            // 마우스 클릭 유지 중일 때
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                OnTouchStay?.Invoke(mousePosition);
-            }
-            // 마우스 클릭 끝났을 때
-            if (Input.GetMouseButtonUp(0))
-            {
-                OnTouchEnd?.Invoke(false);  // 마우스 클릭 끝날 때 isTouch = false 전달
-            }
-        }
-        #endif
+        // #if UNITY_EDITOR       
+        // if(!IsClickUI())
+        // { 
+        //     // 마우스 클릭 시작 시
+        //     if (Input.GetMouseButtonDown(0))
+        //     {
+        //         OnTouchStart?.Invoke(true);  // 마우스 클릭 시작 시 isTouch = true 전달
+        //     }
+        //     // 마우스 클릭 유지 중일 때
+        //     if (Input.GetMouseButton(0))
+        //     {
+        //         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //         OnTouchStay?.Invoke(mousePosition);
+        //     }
+        //     // 마우스 클릭 끝났을 때
+        //     if (Input.GetMouseButtonUp(0))
+        //     {
+        //         OnTouchEnd?.Invoke(false);  // 마우스 클릭 끝날 때 isTouch = false 전달
+        //     }
+        // }
+        // #endif
     }
 
     // UI 위에서 터치 또는 클릭이 이루어졌는지 확인
