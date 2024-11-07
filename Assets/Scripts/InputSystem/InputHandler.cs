be23947d96f521bffef52918cfbf1df5e5d291f3
mangruf0927 <pinky0927@naver.com>
2024-11-07 17:35:48 +0900

    Update : UI 활성화 시 다른 영역 터치 비활성화

--- Assets/Scripts/InputSystem/InputHandler.cs
+++ Assets/Scripts/InputSystem/InputHandler.cs
@@ -10,65 +10,43 @@ public class InputHandler : MonoBehaviour
     public delegate void InputVectorHandler(Vector2 position);
     public event InputVectorHandler OnTouchStay;
 
+    private bool isTouchUI = false;
+
     void Update()
     {
-        if (Input.touchCount > 0 && !IsClickUI())  // 터치가 하나 이상 있는지 확인
+        if (Input.touchCount > 0)  // 터치가 하나 이상 있는지 확인
         {
             Touch touch = Input.GetTouch(0);  // 첫 번째 터치 가져오기
+            Debug.Log(EventSystem.current.IsPointerOverGameObject());
+            
 
             // 터치 시작 상태일 때
             if (touch.phase == TouchPhase.Began)
             {
+                isTouchUI = EventSystem.current.IsPointerOverGameObject();  // 터치가 UI 위에서 시작되었는지 기록
+                if (isTouchUI) return;  // UI 터치 시 이벤트 중지
+
                 OnTouchStart?.Invoke(true);  
             }
             // 터치가 유지되거나 움직였을 때
-            if (touch.phase == TouchPhase.Moved || touch.phase == UnityEngine.TouchPhase.Stationary)
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
             {
+                if (isTouchUI) return;
+
                 Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
                 OnTouchStay?.Invoke(touchPosition);
             }
             // 터치가 끝났을 때
-            if (touch.phase == TouchPhase.Ended)
+            else if (touch.phase == TouchPhase.Ended)
             {
+                if (isTouchUI) 
+                {
+                    isTouchUI = false;  // 상태 초기화
+                    return;  // UI 터치가 끝났을 때 이벤트 중지
+                }   
+
                 OnTouchEnd?.Invoke(false);  
             }
         }
-        
-        #if UNITY_EDITOR       
-        if(!IsClickUI())
-        { 
-            // 마우스 클릭 시작 시
-            if (Input.GetMouseButtonDown(0))
-            {
-                OnTouchStart?.Invoke(true);  // 마우스 클릭 시작 시 isTouch = true 전달
-            }
-            // 마우스 클릭 유지 중일 때
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                OnTouchStay?.Invoke(mousePosition);
-            }
-            // 마우스 클릭 끝났을 때
-            if (Input.GetMouseButtonUp(0))
-            {
-                OnTouchEnd?.Invoke(false);  // 마우스 클릭 끝날 때 isTouch = false 전달
-            }
-        }
-        #endif
-    }
-
-    // UI 위에서 터치 또는 클릭이 이루어졌는지 확인
-    private bool IsClickUI()
-    {
-        // 모바일일 경우
-        if (Input.touchCount > 0)
-        {
-            return EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
-        }
-        #if UNITY_EDITOR
-        
-        // PC(에디터)일 경우
-        return EventSystem.current.IsPointerOverGameObject();
-        #endif
     }
 }
